84a02b305e6dfe45c2f5aeb0c8e0a5085dbe3672
Sergei <gannochenko.sv@gmail.com>
2019-07-18 14:30:27 +0200

    dockerfile renamed

--- generators/app/templates/codebase/src/api/home.js
+++ generators/app/templates/codebase/src/api/home.js
@@ -4,7 +4,7 @@ const useHomeAPI = app => {
     app.get(
         '/',
         wrapError(async (req, res) => {
-            res.status(200).send('Hello from <%- applicationName %>');
+            return res.status(200).send('Hello from <%- applicationName %>');
         }),
     );
 };
