b32b8e1b6489d6b602737be2b1ffdaf6362aafd4
Sergei <gannochenko.sv@gmail.com>
2019-07-21 18:59:48 +0200

    typo fixed

--- generators/app/templates/codebase/script/image.push.heroku.sh
+++ generators/app/templates/codebase/script/image.push.heroku.sh
@@ -5,7 +5,7 @@
 ## Don't forget to authenticate with:
 # heroku login -i
 
-docker login --username=_ --password=`heroku auth:token 2> stdout` registry.heroku.com
+docker login --username=_ --password=`heroku auth:token 2> /dev/null` registry.heroku.com
 
 docker build -t registry.heroku.com/<%- vendorNameKebab %>-<%- applicationCodeKebab %>/web -f docker/production.dockerfile .
 docker push registry.heroku.com/<%- vendorNameKebab %>-<%- applicationCodeKebab %>/web
