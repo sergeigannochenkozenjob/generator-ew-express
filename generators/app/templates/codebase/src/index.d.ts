030b27102334659701d9ef31e33f27d0fd568d73
Sergei <gannochenko.sv@gmail.com>
2019-07-28 22:54:44 +0200

    microfix

--- generators/app/templates/codebase/src/index.d.ts
+++ generators/app/templates/codebase/src/index.d.ts
@@ -7,19 +7,11 @@ type Scalar = string | number | boolean;
 
 // used when it is required to tell that the passed object contains of string keys, and that is it
 interface StringToNullableObjectMap {
-    [s: string]: Nullable<object>;
+    [key: string]: Nullable<object>;
 }
 
-declare module '*.svg' {
+declare module '*.graphql' {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const content: any;
     export default content;
 }
-
-declare module '*.png' {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const content: any;
-    export default content;
-}
-
-declare global {}
