9d76133d192b1d5a4b6a8b0f055f6649216fbcf9
Sergei <gannochenko.sv@gmail.com>
2019-08-18 21:47:05 +0200

    globals fix

--- generators/app/templates/codebase/src/global.d.ts
+++ generators/app/templates/codebase/src/global.d.ts
@@ -1,12 +1,12 @@
-/* eslint-disable import/no-duplicates */
+/* eslint-disable import/no-duplicates, import/order */
 import lodash from './lib/lodash';
 
 declare global {
-    import { logger } from 'ew-internals';
+    import { logger as logger_ } from 'ew-internals';
     import lodash from './lib/lodash';
 
     declare const _ = lodash;
-    declare const logger = logger;
+    declare const logger = logger_;
     declare const __DEV__;
     declare const __TEST__;
 }
